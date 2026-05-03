8981f33f2ace6aefbec9542c3e54f59c89d0d992
ParadoxicXenos <mehroznajif88@gmail.com>
2026-05-03 14:16:35 +1200

    printed hello world. taking a break for a bit

--- boot.asm
+++ boot.asm
@@ -1,24 +1,20 @@
 [org 0x7c00]
+mov ah, 0x0e
+mov bx, greetString
 
-mov bx, 0x7e00
-
-mov ah, 0x02
-mov al, 1
-mov ch, 0
-mov cl, 2
-mov dh, 0
-mov dl, 0x80
-int 0x13
-
-jc disk_error
-
-jmp 0x0000:0x7e00
-
-disk_error:
-    mov ah, 0x0e
-    mov al, 'E'
+printString:
+    mov al, [bx]
+    cmp al, 0
+    je end
     int 0x10
-    jmp $
+    inc bx
+    jmp printString
+end:
+
+jmp $
+
+greetString:
+    db "Hello, world", 0
 
 times 510-($-$$) db 0
-dw 0xaa55
+db 0x55, 0xaa
--- stage2.asm
+++
@@ -1,9 +0,0 @@
-[org 0x7c00]
-mov ah, 0x0e
-mov bx, [greeterString]
-int 0x10
-jmp $==
-
-greeterString:
-    db "Hello, world!",0
-    
