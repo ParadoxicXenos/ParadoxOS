3801d75ee80c8111c3c2bf598f4c49be6213b6a4
ParadoxicXenos <mehroznajif88@gmail.com>
2026-05-05 13:11:56 +1200

    print function created, next is to make new line work

--- kernel.cpp
+++ kernel.cpp
@@ -1,24 +1,27 @@
+//====================================
 #include <stdint.h>
+//====================================
 void print(const char chars[], int len);
+int cursor_row = 0;
+int cursor_col = 0;
+//====================================
 extern "C" void main() {
-    const char msg[] = "Hello, World!";
-
-    print(msg, sizeof(msg) - 1);
-
+    print("Hello", 5);
+    print("World", 5);
     while (1) {}
 }
+
 void print(const char chars[], int len) {
     uint16_t* v_mem = (uint16_t*) 0xB8000;
 
     for (int i = 0; i < len; i++) {
-        v_mem[i] = (0x01 << 8) | chars[i];
+        int cursor_pos = cursor_row * 80 + cursor_col;
+        v_mem[cursor_pos] = (0x01 << 8) | chars[i];
+        cursor_col++;
     }
 }
 
-void splash(){
-    const char msg[] = "@@@@@@@\                                    @@\                      @@@@@@\   @@@@@@\  ";
-    print(msg, sizeof(msg) - 1);
-    const char msg[] = "@@  __@@\                                   @@ |                    @@  __@@\ @@  __@@\ ";
-    print(msg, sizeof(msg) - 1);
-    
+void new_line() {
+    cursor_row++;
+    cursor_col = 0;
 }
