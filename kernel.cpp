617812e23414258f04e7a62a69d087f1ee04eaef
ParadoxicXenos <mehroznajif88@gmail.com>
2026-05-05 14:39:40 +1200

    Add splash screen and improve print functionality  along with code readability

--- kernel.cpp
+++ kernel.cpp
@@ -2,12 +2,13 @@
 #include <stdint.h>
 //====================================
 void print(const char chars[], int len);
+void printl(const char* chars);
+void splash();
 int cursor_row = 0;
 int cursor_col = 0;
 //====================================
 extern "C" void main() {
-    print("Hello", 5);
-    print("World", 5);
+    splash();
     while (1) {}
 }
 
@@ -16,12 +17,35 @@ void print(const char chars[], int len) {
 
     for (int i = 0; i < len; i++) {
         int cursor_pos = cursor_row * 80 + cursor_col;
-        v_mem[cursor_pos] = (0x01 << 8) | chars[i];
+        v_mem[cursor_pos] = (0x09 << 8) | (chars[i] & 0xFF);
         cursor_col++;
+        if (cursor_col >= 80) {
+            cursor_col = 0;
+            cursor_row++;
+        }
     }
 }
 
 void new_line() {
     cursor_row++;
     cursor_col = 0;
+}
+
+void printl(const char* chars) {
+    int len = 0;
+    while (chars[len] != '\0') len++;
+
+    print(chars, len);
+    new_line();
+}
+
+void splash(){
+    printl("______                   _           _____ _____ ");
+    printl("| ___ \\                 | |         |  _  /  ___|");
+    printl("| |_/ /_ _ _ __ __ _  __| | _____  _| | | \\ `--. ");
+    printl("|  __/ _` | '__/ _` |/ _` |/ _ \\ \\/ / | | |`--. \\");
+    printl("| | | (_| | | | (_| | (_| | (_) >  <\\ \\_/ /\\__/ /");
+    printl("\\_|  \\__,_|_|  \\__,_|\\__,_|\\___/_/\\_\\\\___/\\____/");
+    printl("Hello, world!");
+    printl("Program made by ParadoxicXenos");
 }
